d8ed681c327888d879bf4aeaa044ed44d038bf8a
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2024-07-11 22:25:32 +0300

    Widget: Android implementation - first steps

---
+++ android/app/src/main/java/com/mpeiapp/ScheduleWidget.java
@@ -0,0 +1,58 @@
+package com.mpeiapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.content.SharedPreferences;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link ScheduleWidgetConfigureActivity ScheduleWidgetConfigureActivity}
+ */
+public class ScheduleWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        try {
+            SharedPreferences sharedPref = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
+            String appString = sharedPref.getString("appData", "{\"schedule\":'no data'}");
+            JSONObject appData = new JSONObject(appString);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.schedule_widget);
+            views.setTextViewText(R.id.appwidget_text, appData.getString("schedule"));
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            ScheduleWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
---
+++ android/app/src/main/java/com/mpeiapp/SharedStorage.java
@@ -0,0 +1,48 @@
+package com.mpeiapp;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+public class SharedStorage extends ReactContextBaseJavaModule {
+    ReactApplicationContext context;
+
+    public SharedStorage(ReactApplicationContext reactContext) {
+        super(reactContext);
+        context = reactContext;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "SharedStorage";
+    }
+
+    @ReactMethod
+    public void set(String message) {
+        SharedPreferences.Editor editor = context.getSharedPreferences("DATA", Context.MODE_PRIVATE).edit();
+        editor.putString("appData", message);
+        editor.commit();
+
+        Intent intent = new Intent(Objects.requireNonNull(getCurrentActivity()).getApplicationContext(), ScheduleWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] ids = AppWidgetManager.getInstance(getCurrentActivity().getApplicationContext()).getAppWidgetIds(new ComponentName(getCurrentActivity().getApplicationContext(), ScheduleWidget.class));
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        getCurrentActivity().getApplicationContext().sendBroadcast(intent);
+
+    }
+}
