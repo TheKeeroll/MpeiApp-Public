de2c506d99f6ec5ca17250105dbeff947947b79c
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2024-08-02 17:37:59 +0300

    Widget: iOS implementation - first steps

---
+++ ios/ScheduleWidget/ScheduleWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ScheduleWidgetBundle.swift
+//  ScheduleWidget
+//
+//  Created by DragonSavA on 02.08.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ScheduleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ScheduleWidget()
+    }
+}
