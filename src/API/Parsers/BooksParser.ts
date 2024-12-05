533cdcabcc8ee307234f40dbd5f1d859f096b7d5
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2024-12-05 23:35:30 +0300

    Books Parser&Screen fixes&improvements

--- src/API/Parsers/BooksParser.ts
+++ src/API/Parsers/BooksParser.ts
@@ -8,16 +8,27 @@ export default function(raw: string): BARSBooksPack | BARSError {
     const books_list: BARSBook[] = []
     const $ = parse(raw).querySelectorAll('#tbl__PartialListStudent__BookCopy > tbody > tr')//!.querySelectorAll('tr')
     for(let i of $){
+      let raw_book_name = i.querySelector(`td:nth-child(1)`)!.text.trim()
+      if (raw_book_name.includes('просрочено')){
+        raw_book_name = raw_book_name.replace('просрочено', '').trim()
+        console.log('raw_book_name = ' + raw_book_name);
+      }
       const book: BARSBook = {
-        name: i.querySelector(`td:nth-child(1)`)!.text.trim(),
+        name: raw_book_name,
         author: i.querySelector(`td:nth-child(2)`)!.text.trim(),
         code: i.querySelector(`td:nth-child(3)`)!.text.trim(),
         return_until: i.querySelector(`td:nth-child(4)`)!.text.trim(),
       }
       books_list.push(book)
     }
-    const raw_library_card = parse(raw).querySelector('#div-Student__BookCopy > div > div > div > span')!.text.trim()
-    console.log(raw_library_card)
+    let raw_library_card = 'i n i t | empty'
+    try {
+      raw_library_card = parse(raw).querySelector('#div-Student__BookCopy > div > div > div > span')!.text.trim()
+      console.log(raw_library_card)
+    }catch (e: any){
+      console.warn("raw_library_card failed: " + e.toString())
+    }
+
     const result: BARSBooksPack = {
       books: books_list,
       library_card: raw_library_card.split(' ')[5],
