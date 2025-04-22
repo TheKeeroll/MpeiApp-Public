6893869ddb559f61b1a913f8e4eacba6fd5e9b52
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2025-04-22 18:18:20 +0300

    undefined library_card handled

--- src/Screens/BARS/Books/BooksScreen.tsx
+++ src/Screens/BARS/Books/BooksScreen.tsx
@@ -91,7 +91,7 @@ const BooksScreen: React.FC<{navigation: any, params: any}> = (props) => {
                 Styles.libraryCardViewText,
                 {color: colors.text}
               ]}>
-              {booksPack.data?.library_card == 'empty' ? ' ' : ('Читательский билет ' + booksPack.data?.library_card)}
+              {booksPack.data?.library_card == 'empty' ? ' ' : (typeof booksPack.data?.library_card == 'undefined' ? 'Читательский билет не обнаружен...' : 'Читательский билет ' + booksPack.data?.library_card)}
             </Text>
           </View>
         </View>
