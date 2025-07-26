7c9312a60fa3028f6e7a18b314c8b0cbc26e3140
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2025-07-26 19:24:01 +0300

    The Way to 2.0.0 - Step 7
    Предварительная версия исправлений LoadingScreen

--- src/Screens/LoadingScreen/LoadingScreen.tsx
+++ src/Screens/LoadingScreen/LoadingScreen.tsx
@@ -1,25 +1,65 @@
 import React from "react";
 import {ViewStyle} from "react-native";
 import LottieView from "lottie-react-native";
+import { AnimationObject } from "lottie-react-native";
 import {useTheme} from "react-native-paper";
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import {CustomTheme} from "../../Themes/Themes";
 
+import LoadingAnimation from '../../../assets/animations/loading.json';
+
+export const getAnimationKeypaths = (animations: AnimationObject[] | AnimationObject) => {
+    let keypaths: Set<string> = new Set();
+
+    if(!Array.isArray(animations)) {
+        animations.layers.map((l , i) => keypaths.add(l.nm));
+    } else {
+        animations.map((a, i) => {
+            a.layers.map((l , i) => keypaths.add(l.nm));
+        })
+    }
+
+    return Array.from(keypaths);
+}
+
 const LoadingScreen: React.FC<{style?: ViewStyle}> = (props) => {
+    const source = LoadingAnimation;
     const {colors} = useTheme<CustomTheme>()
     const insets = useSafeAreaInsets();
-    return(
-        <SafeAreaView style={[{flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: insets.top, paddingBottom: insets.bottom, height: '100%', width: '100%', backgroundColor: colors.background}, props.style]}>
+    return (
+        <SafeAreaView
+            style={[
+                {
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    paddingTop: insets.top,
+                    paddingBottom: insets.bottom,
+                    backgroundColor: colors.background,
+                },
+                props.style,
+            ]}
+        >
             <LottieView
                 loop
                 autoPlay
-                speed={0.5}
-                resizeMode={"center"}
-                style={{backgroundColor: colors.textUnderline}}
-                source={require('../../../assets/animations/loading.json')}
+                speed={0.66}
+                resizeMode="contain"
+                /*colorFilters={getAnimationKeypaths(source).map(
+                    (path, i) => (
+                        console.log(path),
+                        {
+                            keypath: "Modular_2 (Motion)",
+                            color: (i % 2 === 0) ? colors.textUnderline : colors.text
+                        }
+                    )
+                )}*/
+                style={{ width: 350, height: 350 }} // Задано явно!
+                // source={require('../../../assets/animations/loading.json')}
+                source={source}
             />
         </SafeAreaView>
-    )
+    );
 }
 
 export default LoadingScreen
