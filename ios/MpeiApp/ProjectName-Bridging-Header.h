7dd0d150e38fe823849c84577949b3972160b558
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2025-08-09 19:58:58 +0300

    The Way to 2.0.0 - Step 16
    Различные изменения iOS-части проекта для успешной сборки в XCode.

--- ios/MpeiApp/ProjectName-Bridging-Header.h
+++
@@ -1,1 +0,0 @@
-#import <YandexMapsMobile/YMKMapKitFactory.h>
---
+++ ios/MpeiApp/main.m
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import "MpeiApp-Swift.h" // имя проекта + "-Swift.h" (Xcode сам генерирует)
+
+// Точка входа
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
