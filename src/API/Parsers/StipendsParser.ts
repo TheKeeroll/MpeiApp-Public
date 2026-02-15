a5903fcaf79a16acc213a6a706ff5f671eb98b51
DragonSavA <77333497+DragonSavA@users.noreply.github.com>
2026-02-15 20:46:02 +0300

    Stipend Parser updated
    Актуализация и повышение устойчивости парсера стипендий и заявлений на стипендии.

--- src/API/Parsers/StipendsParser.ts
+++ src/API/Parsers/StipendsParser.ts
@@ -8,30 +8,37 @@ export default function(raw: string): BARSStipendsPack | BARSError {
     const stipends: BARSStipend[] = []
     const $ = parse(raw).querySelectorAll('#div-Student__ScholarshipEvent > div > table > tbody > tr')
     for(let i of $){
-      const stipend: BARSStipend = {
-        start_date: i.querySelector(`td:nth-child(2)`)!.text.trim(),
-        end_date: i.querySelector(`td:nth-child(3)`)!.text.trim(),
-        type: i.querySelector(`td:nth-child(4)`)!.text.trim(),
-        amount: i.querySelector(`td:nth-child(5)`)!.text.trim(),
-        order_date: i.querySelector(`td:nth-child(6)`)!.text.trim(),
-        order_number: i.querySelector(`td:nth-child(7)`)!.text.trim()
+      try{
+        const stipend: BARSStipend = {
+          start_date: i.querySelector(`td:nth-child(1)`)!.text.trim(),
+          end_date: i.querySelector(`td:nth-child(2)`)!.text.trim(),
+          type: i.querySelector(`td:nth-child(3)`)!.text.trim(),
+          amount: i.querySelector(`td:nth-child(4)`)!.text.trim(),
+          order_date: i.querySelector(`td:nth-child(5)`)!.text.trim(),
+          order_number: i.querySelector(`td:nth-child(6)`)!.text.trim()
+        }
+        stipends.push(stipend)
+      } catch (e: any){
+        console.warn(`stipend ${i.textContent} parsing failed: ` + e.toString());
       }
-      stipends.push(stipend)
     }
     const petitions: BARSStipendPetition[] = []
     const $2 = parse(raw).querySelectorAll('#div-Student__ScholarshipStatement > div > table > tbody > tr')
     for(let j of $2){
-      const petition: BARSStipendPetition = {
-        term: j.querySelector(`td:nth-child(1)`)!.text.trim(),
-        wave: j.querySelector(`td:nth-child(2)`)!.text.trim(),
-        type: j.querySelector(`td:nth-child(3)`)!.text.trim(),
-        sub_date: j.querySelector(`td:nth-child(4)`)!.text.trim(),
-        average_grade: j.querySelector(`td:nth-child(6)`)!.text.trim(),
-        total: j.querySelector(`td:nth-child(10)`)!.text.trim(),
-        calc_date: j.querySelector(`td:nth-child(11)`)!.text.trim(),
+      try {
+        const petition: BARSStipendPetition = {
+          term: j.querySelector(`td:nth-child(1)`)!.text.trim(),
+          wave: j.querySelector(`td:nth-child(2)`)!.text.trim(),
+          type: j.querySelector(`td:nth-child(3)`)!.text.trim(),
+          sub_date: j.querySelector(`td:nth-child(4)`)!.text.trim(),
+          average_grade: j.querySelector(`td:nth-child(6)`)!.text.trim(),
+          total: j.querySelector(`td:nth-child(10)`)!.text.trim(),
+          calc_date: j.querySelector(`td:nth-child(11)`)!.text.trim(),
+        }
+        petitions.push(petition)
+      } catch (e: any){
+        console.warn(`petition ${j.textContent} parsing failed: ` + e.toString());
       }
-      petitions.push(petition)
-      console.log(petition)
     }
     const res: BARSStipendsPack = {
       stipends: stipends,
